0716ce42e4920eba5f8332722c9d767a283c1c16
rahul <rahulsalinskee@gmail.com>
2024-11-11 05:15:29 +0530

    Use Ref | App JS

--- src/App.js
+++ src/App.js
@@ -3,8 +3,10 @@ import { ContainerComponent } from './Components/ContainerComponent';
 import InputForm from './Components/Forms/ControlledComponent/InputFormControlledComponent';
 import { ThemeChange } from './Events/ThemeChange';
 import { CounterUseEffect } from './Hooks/UseEffect/CounterUseEffect';
+import { MessageLetterCountUsingUseState } from './Hooks/UseRef/MessageLetterCountUsingUseState';
 import { Counter } from './Hooks/UseState/Counter';
 import { DigitalClock } from './Hooks/UseState/DigitalClock';
+import { MessageLetterCountUsingUseRef } from './Hooks/UseRef/MessageLetterCountUsingUseRef';
 
 
 function App() {
@@ -16,7 +18,9 @@ function App() {
       {/* <DigitalClock /> */}
       {/* <ThemeChange /> */}
       {/* <InputForm /> */}
-      <CounterUseEffect />
+      {/* <CounterUseEffect /> */}
+      {/* <MessageLetterCountUsingUseState /> */}
+      <MessageLetterCountUsingUseRef />
     </>
   );
 }
