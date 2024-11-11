25d82b972f4eeff1e2a35cf984fe8606cb766e28
rahul <rahulsalinskee@gmail.com>
2024-11-11 14:50:04 +0530

    Comment updated for the solution of Prop drilling problem
    SOLUTION: As a part of the solution for Prop Drilling, we have 3 ways to solve this problem.
     * 1 way - Use of Context API - Create Context | Provider | Consumer (Here, consumer will also have complex code for which consumer eventually will be replaced by UseContext hook)
    
     * 2 Way - Use of Use Context Hook in Consumer (Of Context APi. - Create Context | Provider will remain same)
    
     * 3 Way - Use of external library - Redux
     ----------------
     This approach is using context API
    
     Context API - There are 3 important things in context API
     * A. Create Context
     * B. Provider (a. Use Name Tag for the context we created in step 1 and write the tag in Return statement b. export this name tag)
     * C. Consumer

--- src/Hooks/UseContext/PropDrilling/Solution/A.jsx
+++ src/Hooks/UseContext/PropDrilling/Solution/A.jsx
@@ -1,14 +1,15 @@
 import React, { createContext } from 'react';
 import { B } from './B';
 
-/* ***** SOLUTION: As a part of the solution for Prop Drilling, we have 2 ways to solve this problem. 
- * 1 way - Use of Context API
- * 2 Way - Use of external library - Redux
+/* ***** SOLUTION: As a part of the solution for Prop Drilling, we have 3 ways to solve this problem. 
+ * 1 way - Use of Context API - Create Context | Provider | Consumer 
+ * 2 Way - Use of Use Context Hook instead of Consumer (Of Context APi. - Create Context & Provider will remain same)
+ * 3 Way - Use of external library - Redux
  * This approach is using context API
  * Context API - There are 3 important things in context API 
  * A. Create Context
  * B. Provider (a. Use Name Tag for the context we created in step 1 and write the tag in Return statement b. export this name tag)
- * C. Consumer
+ * C. Consumer (Here, consumer will also have complex code for which consumer eventually will be replaced by UseContext hook)
 ***** */
 
 /* *****
