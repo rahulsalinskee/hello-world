cf07e22febf2b0ddec71c0735e027b1e1b4d1d1a
rahul <rahulsalinskee@gmail.com>
2024-11-25 11:18:26 +0530

    Add Home Page Button Link to navigate on Home page from Error Page.

--- src/Pages/ErrorPage.jsx
+++ src/Pages/ErrorPage.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function ErrorPage() {
+    const navigateTo = useNavigate();
+    const homePagePath = './home';
+
+    function HomeButtonOnClickEventHandler(event) {
+        event.preventDefault();
+        navigateTo(homePagePath);
+    }
+
     return (
         <>
             <h3>Sorry! Page Not Found! : 404 Error</h3>
+            <button onClick={HomeButtonOnClickEventHandler}>Home</button>
         </>
     )
 }
