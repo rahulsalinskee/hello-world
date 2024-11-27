af2cef2f36b588e61f211ae467c88c3e9dffcdd5
rahul <rahulsalinskee@gmail.com>
2024-11-27 23:00:56 +0530

    Adding Home Use Deferred Value Hook

--- src/Pages/UseDeferredValueHookPage.jsx
+++ src/Pages/UseDeferredValueHookPage.jsx
@@ -1,10 +1,18 @@
-import React from 'react'
-import { Outlet, NavLink } from 'react-router-dom'
+import React from 'react';
+import { Outlet, NavLink, useNavigate } from 'react-router-dom';
 
 export default function UseDeferredValueHookPage() {
+    const navigateTo = useNavigate();
+    const homePagePath = '../home';
+
+    function NavigateToHomeButtonClickHandler(event) {
+        event.preventDefault();
+        navigateTo(homePagePath);
+    }
     return (
         <>
             <h3>Use Deferred Value Hook Page</h3>
+            <button style={{ marginTop: '2rem' }} onClick={NavigateToHomeButtonClickHandler}>Home</button>
             <div style={{ display: 'flex', flexDirection: 'row', gap: '2rem' }}>
                 <NavLink to='similar-problem-like-use-transition-without-using-use-deferred-value-hook-application'>Without using Use Deferred Value Application</NavLink>
                 <NavLink to='similar-problem-solution-like-use-transition-using-use-deferred-value-hook-application'>Using Use Deferred Value Application</NavLink>
